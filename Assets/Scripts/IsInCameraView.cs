a4c65b3ba929076d1958b61e1b140d739265de68
Zabilya <36747171+Zabilya@users.noreply.github.com>
2020-03-31 17:29:50 +0300

    add IsInCameraViewScript

---
+++ Assets/Scripts/IsInCameraView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsInCameraView : MonoBehaviour
+{
+    public bool isInView;
+    private Renderer _objRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _objRenderer = this.gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_objRenderer.IsVisibleFrom(Camera.main))
+        {
+            isInView = true;
+        }
+    }
+}
