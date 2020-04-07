f00cfcc27c775e7fcf2b58015ef21372c5ee29b7
Zabilya <36747171+Zabilya@users.noreply.github.com>
2020-04-07 17:12:19 +0300

    add working teleporter with one bug

--- Assets/Scripts/portalScripts/Portal.cs
+++ Assets/Scripts/portalScripts/Portal.cs
@@ -11,7 +11,7 @@ namespace portalScripts
         void Start()
         {
             otherPortal.portalView.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-            GetComponent<MeshRenderer>().sharedMaterial.mainTexture = otherPortal.portalView.targetTexture;
+            GetComponentInChildren<MeshRenderer>().sharedMaterial.mainTexture = otherPortal.portalView.targetTexture;
         }
 
         // Update is called once per frame
@@ -20,11 +20,13 @@ namespace portalScripts
             // Position
             Vector3 playerPosition =
                 otherPortal.transform.worldToLocalMatrix.MultiplyPoint3x4(Camera.main.transform.position);
-            portalView.transform.localPosition = -playerPosition;
+            playerPosition = new Vector3(-playerPosition.x, playerPosition.y, -playerPosition.z);
+            portalView.transform.localPosition = playerPosition;
             
             // Rotation
             Quaternion difference = transform.rotation * 
-                                    Quaternion.Inverse(otherPortal.transform.rotation * Quaternion.Euler(0, 180, 0));
+                                    Quaternion.Inverse(otherPortal.transform.rotation * 
+                                                       Quaternion.Euler(0, 180, 0));
             portalView.transform.rotation = difference * Camera.main.transform.rotation;
             
             // Clipping
---
+++ Assets/Scripts/portalScripts/Teleporter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+
+namespace portalScripts
+{
+    public class Teleporter : MonoBehaviour
+    {
+        private static readonly Quaternion halfTurn = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+        public Teleporter otherTeleporter;
+        public GameObject player;
+        public int teleportedNum;
+        public bool isTeleported;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            player = GameObject.Find("Player");
+            teleportedNum = 0;
+        }
+
+        private void FixedUpdate()
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+
+        // Update is called once per frame
+        // void LateUpdate()
+        // {
+        //     if (isTeleported)
+        //     {
+        //         if (teleportedNum > 3)
+        //         {
+        //             player.GetComponent<PlayerController>().enabled = true;
+        //             teleportedNum = 0;
+        //         }
+        //
+        //         teleportedNum++;
+        //     }
+        // }
+
+        private void OnTriggerStay(Collider other)
+        {
+            float zPos = transform.worldToLocalMatrix.MultiplyPoint3x4(other.transform.position).z;
+
+            if (zPos < 0)
+            {
+                player.GetComponent<PlayerController>().enabled = false;
+                Teleport(player.GetComponent<Transform>());
+                isTeleported = true;
+            }
+        }
+
+        private void Teleport(Transform objTrans)
+        {
+            // Position
+            Vector3 localPos = transform.worldToLocalMatrix.MultiplyPoint3x4(objTrans.position);
+            localPos = new Vector3(-localPos.x, localPos.y, -localPos.z);
+            objTrans.position = otherTeleporter.transform.localToWorldMatrix.MultiplyPoint3x4(localPos);
+            
+            
+            // // Rotation
+            // Quaternion difference = transform.rotation * Quaternion.Inverse(otherTeleporter.transform.rotation * 
+            //                                                                Quaternion.Euler(0, 180, 0)); 
+            // objTrans.rotation = difference * objTrans.rotation;
+            
+            // Update position of object.
+            // Vector3 relativePos = transform.InverseTransformPoint(objTrans.position);
+            // relativePos = halfTurn * relativePos;
+            // objTrans.position = otherTeleporter.transform.TransformPoint(relativePos);
+
+            // Update rotation of object.
+            Quaternion relativeRot = Quaternion.Inverse(transform.rotation) * objTrans.rotation;
+            relativeRot = halfTurn * relativeRot;
+            objTrans.rotation = otherTeleporter.transform.rotation * relativeRot;
+        }
+    }
+}
